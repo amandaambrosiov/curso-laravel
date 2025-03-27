c4aeb0e0a7212543f5b12545b3f79f81d19a1e96
Amanda Ambrosio <amandaambrosio295@gmail.com>
2025-03-27 14:54:58 -0300

    Inserindo opção de deletar

--- app-laravel/app/Http/Controllers/Admin/SupportController.php
+++ app-laravel/app/Http/Controllers/Admin/SupportController.php
@@ -66,5 +66,13 @@ class SupportController extends Controller
 
     }
 
-    
+    public function destroy(string|int $id)
+    {
+        if (!$support = Support::find($id)) {
+            return back();
+        }
+        $support->delete();
+
+        return redirect()->route('supports.index');
+    }
 }
