81c3f2df09092e6ae37537e8babf331e0767155c
Amanda Ambrosio <amandaambrosio295@gmail.com>
2025-03-28 14:01:36 -0300

    corrigindo bugs

--- app-laravel/app/Repositories/SupportEloquentORM.php
+++ app-laravel/app/Repositories/SupportEloquentORM.php
@@ -14,35 +14,36 @@ class SupportEloquentORM implements SupportRepositoryInterface
 {
     public function __construct(
         protected Support $model
-    ){}
+    ) {}
 
     public function getAll(?string $filter = null): array
     {
         return $this->model
-                    ->where(function ($query) use ($filter){
-                        if ($filter){
-                            $query->where('subject', $filter);
-                            $query->orwhere('body', 'like', "%{$filter}%");
-                        }
-                    })
-                    ->get()
-                    ->toArray();
+            ->where(function ($query) use ($filter) {
+                if ($filter) {
+                    $query->where('subject', $filter);
+                    $query->orwhere('body', 'like', "%{$filter}%");
+                }
+            })
+            ->get()
+            ->toArray();
     }
 
     public function findOne(string $id): stdClass|null
     {
         $support = $this->model->find($id);
-        if(!$support){
+        if (!$support) {
             return null;
         }
 
         return (object) $support->toArray();
-
     }
+
     public function delete(string $id): void
     {
-        $this->model->findOrFail()->delete();
+        $this->model->findOrFail($id)->delete();
     }
+
     public function new(CreateSupportDTO $dto): stdClass
     {
 
@@ -50,11 +51,11 @@ class SupportEloquentORM implements SupportRepositoryInterface
             (array) $dto
         );
 
-        return (object) $support->toArray;
+        return (object) $support->toArray();
     }
     public function update(UpdateSupportDTO $dto): stdClass|null
     {
-        if ($support = $this->model->find($dto->id)){
+        if ($support = $this->model->find($dto->id)) {
             return null;
         }
 
@@ -63,6 +64,5 @@ class SupportEloquentORM implements SupportRepositoryInterface
         );
 
         return (object) $support->toArray();
-
     }
 }
