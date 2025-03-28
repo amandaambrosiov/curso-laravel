69e97767b65cdbbe9d0fc25c6bb3a812d3829a22
Amanda Ambrosio <amandaambrosio295@gmail.com>
2025-03-28 10:19:38 -0300

    adcionando service layer

--- app-laravel/app/Http/Controllers/Admin/SupportController.php
+++ app-laravel/app/Http/Controllers/Admin/SupportController.php
@@ -5,13 +5,19 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreUpdateSupport;
 use App\Models\Support;
+use App\Services\SupportService;
 use Illuminate\Http\Request;
 
 class SupportController extends Controller
 {
-    public function index(Support $support)
+
+    public function __construct(
+        protected SupportService $service)
+    {}
+
+    public function index(Request $request)
     {
-        $supports = $support->all();
+        $supports = $this->service->getAll($request->filter);
 
         return view('admin/supports/index', compact('supports'));
     }
@@ -22,7 +28,7 @@ class SupportController extends Controller
     {
         //Support::where('id', $id)->first();
         //Support::where('id', '!=' $id)->first();
-        if (!$support = Support::find($id)) {
+        if (!$support = $this->service->findOne($id)) {
             return back();
         }
         
@@ -44,9 +50,10 @@ class SupportController extends Controller
         return redirect()->route('supports.index');
     }
 
-    public function edit(Support $support, string|int $id)
+    public function edit(string $id)
     {
-        if (!$support = $support->where('id', $id)->first()) {
+        //if (!$support = $support->where('id', $id)->first()) {
+        if (!$support = $this->service->findOne($id)) {
             return back();
         }
 
@@ -73,12 +80,9 @@ class SupportController extends Controller
 
     // FUNÇÃO DE DELETAR
 
-    public function destroy(string|int $id)
+    public function destroy(string $id)
     {
-        if (!$support = Support::find($id)) {
-            return back();
-        }
-        $support->delete();
+        $this->service->delete($id);
 
         return redirect()->route('supports.index');
     }
